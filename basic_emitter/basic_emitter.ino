89898b7fcc504423e34a18e5d1ec589752cfec58
Alessandro Jeanteur <aless.jeant@gmail.com>
2018-04-03 23:22:11 +0200

    changed method of modulating manchester

--- basic_emitter/basic_emitter.ino
+++ basic_emitter/basic_emitter.ino
@@ -24,30 +24,30 @@ void setup() {
   pinMode(blueDiodePort, OUTPUT);
   pinMode(okLedPort, OUTPUT);
   pinMode(failLedPort, OUTPUT);
-  blinker(3, failLedPort, 1000);
-  
+  blinker(3, failLedPort, 100);
+
   //for testing, commenting otherwise
   Serial.begin(9600);
-  
+
   byte test1[messageSize];
-  test1[0] = 0xcc;
-  test1[1] = 0x78;
-  
+  test1[0] = 0x0;
+  test1[1] = 0xAA;
+
   uint16_t test2[messageSize];
-  
+
   moduler (test1, test2);
-  
+
   Serial.print(test1[0], BIN);
   Serial.print("\t");
   Serial.print(test1[1], BIN);
   Serial.print("\n");
-  
+
   Serial.print(test2[0], BIN);
   Serial.print("\t");
   Serial.print(test2[1], BIN);
-  Serial.print("\n"); 
+  Serial.print("\n");
 
-  
+
   outBit = digitalPinToBitMask(blueDiodePort);
   outPort = digitalPinToPort(blueDiodePort);
   out = portOutputRegister(outPort); //pourrait remplacer outPort par digitalPintToPort directement....
@@ -55,21 +55,20 @@ void setup() {
 
 void loop() {
   currentMicros = micros();
-  
-  if( 2*(currentMicros-previousMicros) > blinkPeriod) {
+
+  if ( 2 * (currentMicros - previousMicros) > blinkPeriod) {
     ledState ^= 0x01; //toggle
-    
-    
+
+
     if (ledState) {
       *out |= outBit;
-    } 
+    }
     else {
       *out &= ~outBit;
     }
-    
+
     previousMicros = currentMicros;
   }
-
 }
 
 
@@ -78,48 +77,26 @@ void moduler(byte message[messageSize], uint16_t moduledMsg[messageSize])
   byte val;
   int i, j;
   uint16_t inter = 0;
-  
-  for (i=0;i<messageSize;i++)
+  uint16_t mask = 0;
+  for (i = 0; i < messageSize; i++)
   {
-    for (j=0;j<8;j++)
+    for (j = 0; j < 8; j++)
     {
-      val = (message[i]>>j) & 0x01;
-      
-//      Serial.println(val);
-      
-      inter >>= 1; //on passe au bit suivant 
-      //maintenant le bit tout à gauche est à 0
-      
-      inter |= val << 15; //clk XOR msg.
-      // << 15 pour que ça soit sur le bit tout à gauche
-      
-      
-      
-//      Serial.println(inter, BIN);
-      // maintenant le deuxième bit du manchester
-      inter >>= 1;
-      
-      inter |= (val^0x01) << 15;
-            
-//      Serial.println(inter, BIN);
-//      Serial.print("\n");
+      val = (message[i] >> j) & 0x01;
+      mask = 0x03 << 2 * j; // les deux bits qu'on modifie seulement
+      inter = (inter & ~mask) | (0x01 << (2 * j + 1 - val) & mask ); // replace bits under mask by 01 if val = 1, 10 if val = 0
     }
-    
-//    Serial.print("\n");
-//    Serial.print("\n");
-    
-    //Serial.println(inter);
     moduledMsg[i] = inter;
   }
 }
 
 
-void blinker(int times, int port, int period){
-  for (int i=0; i<times; i++) {
+void blinker(int times, int port, int period) {
+  for (int i = 0; i < times; i++) {
     digitalWrite(port, HIGH);
-    delay(period/2);
-    digitalWrite(port,LOW);
-    delay(period/2);
+    delay(period / 2);
+    digitalWrite(port, LOW);
+    delay(period / 2);
   }
 }
 
